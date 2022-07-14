63cc2875f719c0971b1bfa1339fd8e5d87854a51
dazcui <david.azcui@gmail.com>
2022-07-13 21:48:25 -0400

    proyecto

---
+++ data.js
@@ -0,0 +1,24 @@
+export const tarjetas = [
+    {
+        id: 1,
+        titulo: "Hoodies",
+        stock: 9,
+        price:14,
+        imagen:"./imagenes/featured1.png",
+    },
+    {
+        id: 2,
+        titulo: "Shirts",
+        stock: 15,
+        price:24,
+        imagen:"./imagenes/featured2.png",
+    },
+    {
+        id: 3,
+        titulo: "Sweatshirts",
+        stock: 19,
+        price:36,
+        imagen:"./imagenes/featured3.png",
+    },
+    
+];
